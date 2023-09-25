ba523d2d7d20b353aef409ad6f38444a8b4736d4
Flavio Coutinho <coutinhoflavio20@gmail.com>
2023-09-25 13:15:53 +0100

    feat: Create database and migrations

--- WebAPI/Models/FuncionarioModel.cs
+++ WebAPI/Models/FuncionarioModel.cs
@@ -1,7 +1,18 @@
-﻿namespace WebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using WebAPI.Enums;
+
+namespace WebAPI.Models
 {
     public class FuncionarioModel
     {
-
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        public string Sobrenome { get; set; }
+        public DepartamentoEnum Departamento { get; set; }
+        public bool Ativo { get; set; }
+        public TurnoEnum Turno { get; set; }
+        public DateTime DataDaCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDaAlteracao { get; set; } = DateTime.Now.ToLocalTime();
     }
 }
